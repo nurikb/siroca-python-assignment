5c48ca24e2487adf49d2b1ef4846324b17df93d6
nurik <nurikbektursunovb@gmail.com>
2021-11-18 01:06:25 +0600

    add excepts

--- sirocaapp/views.py
+++ sirocaapp/views.py
@@ -18,13 +18,17 @@ def get_index(request):
 
 
 def get_pull_count(url):
+    #   get the number of pull requests
+    #   the count is divided by 30 since there are 30 pull requests in one response
     header = {'User-Agent': user_agency}
     try:
         r = requests.get(url, headers=header)
         soup = bs(r.text, 'lxml')
         count = int(soup.find('div', class_='pt-3 hide-full-screen mb-5').find('a', id='pull-requests-tab').find('span',
-                                                                                                  class_='Counter').text)
-        if count > 30:
+                                                                                                 class_='Counter').text)
+        if count == 0:
+            return 0
+        elif count > 30 and count > 0:
             return count // 30
         else:
             return 1
@@ -37,14 +41,13 @@ async def get_data(session, url, page):
     split_url = url.split('/')
     user_project = split_url[3] + '/' + split_url[4] + f'/pulls?page={page}'
 
-    async with session.post(url=git_api+user_project) as response:
-        print(f'get_data({git_api+user_project})')
+    async with session.get(url=git_api+user_project) as response:
         r_json = await response.json()
-        data.append(r_json)
+        if r_json != []:
+            data.append(r_json)
 
 
 async def create_tasks(url, count):
-    print('create_tasks()')
     tasks = []
 
     async with aiohttp.ClientSession() as session:
@@ -58,7 +61,8 @@ async def create_tasks(url, count):
 def make_request(request):
     form = IndexForm(request.POST)
     try:
-        user_request = UserRequest.objects.get(url=request.POST.get("url", ""))
+        url = request.POST.get("url", "")
+        user_request = UserRequest.objects.get(url=url)
         pulls = UserRequestResult.objects.filter(url=user_request)
         return render(request, 'sirocaapp/index.html', context={'pulls': pulls})
 
@@ -67,30 +71,28 @@ def make_request(request):
         if form.is_valid():
             url = form.cleaned_data['url']
             count = get_pull_count(url)
-            print(count+1)
-
-            asyncio.run(create_tasks(url, count))
-            print(data)
-            if type(data) is dict:
-                return render(request, 'sirocaapp/index.html', context={'empty': data["message"]})
-
-            if not data:
+            print(count)
+            if count == 0:
                 empty = 'There are not pull requests'
                 return render(request, 'sirocaapp/index.html', context={'empty': empty})
 
-            else:
-                form.save()
-                user_request = UserRequest.objects.get(url=url)
-                for data_list in data:
-                    for i in data_list:
-                        user_request_result_model = UserRequestResult()
-                        user_request_result_model.url = user_request
-                        user_request_result_model.pull_name = i["title"]
-                        user_request_result_model.reviewers_name = i["user"]["login"]
-                        user_request_result_model.assignees_name = i["base"]["user"]["login"]
-                        user_request_result_model.pull_url = i["html_url"]
-                        user_request_result_model.save()
-                        pulls = UserRequestResult.objects.filter(url__url=url)
+            asyncio.run(create_tasks(url, count+1))
+
+            try:
+                return render(request, 'sirocaapp/index.html', context={'empty': data[0]["message"]})
+            except TypeError:
+                    form.save()
+                    user_request = UserRequest.objects.get(url=url)
+                    for data_list in data:
+                        for i in data_list:
+                            user_request_result_model = UserRequestResult()
+                            user_request_result_model.url = user_request
+                            user_request_result_model.pull_name = i["title"]
+                            user_request_result_model.reviewers_name = i["user"]["login"]
+                            user_request_result_model.assignees_name = i["base"]["user"]["login"]
+                            user_request_result_model.pull_url = i["html_url"]
+                            user_request_result_model.save()
+                            pulls = UserRequestResult.objects.filter(url__url=url)
             return render(request, 'sirocaapp/index.html', context={'pulls': pulls})
 
         return render(request, 'sirocaapp/index.html', context={'form': form})
