c2a42793b59bd32a02569389f023253c56d65532
nurmuh <56222683+nurikb@users.noreply.github.com>
2021-11-17 17:46:30 +0600

    add models

--- sirocaapp/models.py
+++ sirocaapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class UserRequest(models.Model):
+    url = models.CharField(max_length=150, db_index=True, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.url)
+
+
+class UserRequestResult(models.Model):
+    url = models.ForeignKey(UserRequest, on_delete=models.CASCADE)
+    pull_name = models.CharField(max_length=150, db_index=True, null=True)
+    reviewers_name = models.CharField(max_length=150, db_index=True)
+    assignees_name = models.CharField(max_length=150, db_index=True)
+    pull_url = models.CharField(max_length=150, db_index=True)
--- sirocaapp/views.py
+++ sirocaapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import requests
+
+git_api = 'https://api.github.com/repos/'
 
 
 def get_index(request):
     return render(request, 'sirocaapp/index.html')
+
+
+def get_data(url):
+    split_url = url.split('/')
+    user_project = split_url[3] + '/' + split_url[4] + '/pulls'
+    r = requests.get(git_api+user_project)
+    return r.json()
+
 
